31d657b13af4c3c086d57e8463bb34eed7701f9c
saez_claudio <saez_@DESKTOP-9MAL03L>
2020-05-02 21:33:52 -0300

    agregado smoothscrollbar(desplazamiento del scroll suave)

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,9 +1,8 @@
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
-    $('html, body').animate({
+    $('html, body, #main').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
+    
 });
-
-
