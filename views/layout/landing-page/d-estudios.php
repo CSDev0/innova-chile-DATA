a0f198c3289a986a9a43c42c3e2f221cbb29ee84
saez_claudio <saez_@DESKTOP-9MAL03L>
2020-05-18 15:21:22 -0400

    v0.5.77 - NAVBAR ESTUDIOS- NUESTROS ESTUDIOS- LECTURAS RECOMENDADAS

--- views/layout/landing-page/d-estudios.php
+++ views/layout/landing-page/d-estudios.php
@@ -2,81 +2,144 @@
     <div class="col-sm-12">
         <h1>Estudios</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div id="NuestrosEstudios"></div>
         <br><br>
+        <div class="row" >
+            <div class="col-sm-6">
+                <h2>Nuestros estudios</h2>
+                <br>
+            </div>
+            <div class="col-sm-6">
+                <br>
+            </div>
+        </div> 
         <div class="row">
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta caracterizacion Socioeconomica Nacional (CASEN)</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta caracterizacion Socioeconomica Nacional (CASEN)</a>
                 <hr>
             </div>
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Indicadores de Acceso a la salud</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Indicadores de Acceso a la salud</a>
                 <hr>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios" >Nueva Encuesta Nacional de Empleo (NENE)</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios" >Nueva Encuesta Nacional de Empleo (NENE)</a>
                 <hr>
             </div>
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Esperanza de Vida</a>
-                <hr>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Nueva Encuesta Suplementaria de Ingresos (NESI)</a>
-                <hr>
-            </div>
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Estadisticas Migratorias</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Esperanza de Vida</a>
                 <hr>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Matricula Escolar</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Nueva Encuesta Suplementaria de Ingresos (NESI)</a>
                 <hr>
             </div>
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Nacional de Discapacidad (ENDISC)</a>
-                <hr>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Prueba de Seleccion Universitaria (PSU)</a>
-                <hr>
-            </div>
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Sobre Gasto y Personal en I+D</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Estadisticas Migratorias</a>
                 <hr>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Registro de Exportacion e Importacion</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Matricula Escolar</a>
                 <hr>
             </div>
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Desempeño Escolar</a>
-                <hr>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Causas de Muerte</a>
-            </div>
-            <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Proyecciones de Poblacion</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Nacional de Discapacidad (ENDISC)</a>
                 <hr>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Prueba de Seleccion Universitaria (PSU)</a>
+                <hr>
             </div>
             <div class="col-sm-6">
-                <a class="link-normal" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Nacional de Acceso y Usos de Internet</a>
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Sobre Gasto y Personal en I+D</a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Registro de Exportacion e Importacion</a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Desempeño Escolar</a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Causas de Muerte</a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Proyecciones de Poblacion</a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Encuesta Nacional de Acceso y Usos de Internet</a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+
+            </div>
+        </div>
+        <div id="LecturasRecomendadas"></div>
+        <br><br><br>
+        <div class="row" >
+            <div class="col-sm-6">
+                <h2>Lecturas recomendadas</h2>
+                <br>
+            </div>
+            <div class="col-sm-6">
+                <br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Manual de Frascatti</a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Manual de Oslo</a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Estadísticas e Innicadores de Innovación en Negocios de la OCDE </a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">Observatorio del Ministerio de Economía, Fomento y Turismo</a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">GCR: Índice de Competitividad Global del World Economic  </a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">GII: Índice de Innovación </a>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios">GEM: Global Entrepreneurship  </a>
+                <hr>
+            </div>
+            <div class="col-sm-6">
+                <a class="link-normal estudios" href="#"data-toggle="modal" data-target="#modal-estudios"></a>
             </div>
         </div>
     </div>
--- views/layout/navbar.php
+++ views/layout/navbar.php
@@ -11,24 +11,30 @@
                 <div class="dropdown">
                     <a class="nav-link dropdown-toggle" href="#3">Data</a>
 
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <div class="move-drop">
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <div class="move-drop">
                             <a class="dropdown-item" id="dropdown-item-hover" href="#"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow"></i> Portafolio</a>
                             <a class="dropdown-item"  id="dropdown-item-hover2" href="#"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow2"></i> Ley I+D</a>
                             <div class="dropdown">
                                 <a class="dropdown-item" id="dropdown-item-hover3"href="#"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow3"></i> Subsidios</a>
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" id="dropdown-item-hover4" href="<?=base_url?>web/data"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow4"></i> Convocatoria</a>
-                                    <a class="dropdown-item" id="dropdown-item-hover5" href="<?=base_url?>web/apptest"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow5"></i> Historico</a>
+                                    <a class="dropdown-item" id="dropdown-item-hover4" href="<?= base_url ?>web/data"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow4"></i> Convocatoria</a>
+                                    <a class="dropdown-item" id="dropdown-item-hover5" href="<?= base_url ?>web/apptest"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow5"></i> Historico</a>
                                 </div>
                             </div>
-                            </div>
                         </div>
+                    </div>
 
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#4">Estudios</a>
+                <div class="dropdown">
+                    <a class="nav-link dropdown-toggle" href="#4">Estudios</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" id="dropdown-item-hover6" href="#NuestrosEstudios"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow6"></i> Nuestros estudios</a>
+                        <a class="dropdown-item"  id="dropdown-item-hover7" href="#LecturasRecomendadas"><i class="fa fa-angle-double-right" id="nav-bar-dropdown-arrow7"></i>Lecturas recomendadas</a>
+                    </div>
+                </div>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#5">Chile en el mundo</a>
