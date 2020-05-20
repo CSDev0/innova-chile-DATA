ca47c13509bd006e6f6108e9111a029e2cb2d924
Elias Cordero <elias_isaac@outlook.com>
2020-05-20 15:14:13 -0400

    v0.5.82 - Graficos en carousel con bootstrap.

--- views/layout/landing-page/b-graficos-destacados.php
+++ views/layout/landing-page/b-graficos-destacados.php
@@ -17,41 +17,59 @@
             </div>
         </div>
         <br>
-        <div class="row">
-            <div class="col-sm-4">
-                <div class="contenedor-grafico">
-                    <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
-                    </iframe>
-                </div>
+        <div id="graficos-crsl" class="carousel slide bg-light" data-ride="carousel">
+
+            <!-- Indicators -->
+          <ul class="carousel-indicators bg-primary">
+            <li data-target="#graficos-crsl" data-slide-to="0" class="active"></li>
+            <li data-target="#graficos-crsl" data-slide-to="1"></li>
+            <li data-target="#graficos-crsl" data-slide-to="2"></li>
+            <li data-target="#graficos-crsl" data-slide-to="3"></li>
+            <li data-target="#graficos-crsl" data-slide-to="4"></li>
+          </ul>
+
+          <!-- The slideshow -->
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="contenedor-grafico bg-light">
+                  <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
+                  </iframe>
+              </div>
             </div>
-            <div class="col-sm-4">
-                <div class="contenedor-grafico">
-                    <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
-                    </iframe>
-                </div>
+            <div class="carousel-item">
+              <div class="contenedor-grafico bg-light">
+                  <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
+                  </iframe>
+              </div>
             </div>
-            <div class="col-sm-4">
-                <div class="contenedor-grafico">
-                    <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
-                    </iframe>
-                </div>
+            <div class="carousel-item">
+              <div class="contenedor-grafico bg-light">
+                  <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
+                  </iframe>
+              </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4">
-                <div class="contenedor-grafico">
-                    <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
-                    </iframe>
-                </div>
+            <div class="carousel-item">
+              <div class="contenedor-grafico bg-light">
+                  <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
+                  </iframe>
+              </div>
             </div>
-            <div class="col-sm-4">
-                <div class="contenedor-grafico">
-                    <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
-                    </iframe>
-                </div>
+            <div class="carousel-item">
+              <div class="contenedor-grafico bg-light">
+                  <iframe class="grafico-destacado" src="https://raw.githack.com/Clazherx/innova-chile-DATA/master/plot_test_1.html">
+                  </iframe>
+              </div>
             </div>
-            <div class="col-sm-2"></div>
+          </div>
+
+          <!-- Left and right controls -->
+          <a class="carousel-control-prev" href="#graficos-crsl" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a class="carousel-control-next" href="#graficos-crsl" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </a>
+
         </div>
     </div>
 </div>
