0b061e5b94564749a549d6f4fe9541e17f7dc716
Elias Cordero <elias_isaac@outlook.com>
2020-06-02 18:25:36 -0400

    Prueba nueva rama

--- views/layout/header.php
+++ views/layout/header.php
@@ -19,7 +19,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
- 
+
         <!--Funciones $.debounce: junta todas las peticiones y las ejecuta luego de unos milisegundos.
                                 $.throttle: solo permite la ejecucion de una peticion cada tantos milisegundos.
         -->
@@ -28,7 +28,7 @@
         <script src="<?= base_url ?>assets/js/parameters.js"></script>
         <script src="<?= base_url ?>assets/js/scrollify.js"></script>
         <script src="<?= base_url ?>assets/js/animations.js"></script>
-        
+
 
         <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
         <title>InnovaChile - Proyectos para el mañana.</title>
