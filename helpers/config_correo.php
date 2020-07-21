1d00afaa7821772d3c495b179b3b7fb589ed9741
saez_claudio <saez_@DESKTOP-9MAL03L>
2020-07-20 21:31:26 -0400

    v.0.9.92 - Bug fix credenciales correo.

--- helpers/config_correo.php
+++ helpers/config_correo.php
@@ -7,9 +7,9 @@ $mail->isSMTP();
 $mail->setLanguage('es');
 $mail->CharSet = 'utf-8';
 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-$mail->Host = 'mail.datainnovacion.cl';
+$mail->Host = host_smtp;
 $mail->Port = 587; //587 es el puerto predeterminado para seguridad TLS.
 $mail->SMTPAuth = true; // Necesario autenticarse?.
-$mail->Username = "soporte@datainnovacion.cl"; // Correo GMAIL.
-$mail->Password = "datainnovacion2020@SOPORTE"; // Clave de gmail or App Specific Password.
+$mail->Username = user_smtp; // Correo GMAIL.
+$mail->Password = password_smtp; // Clave de gmail or App Specific Password.
 $mail->IsHTML(true); //El contenido usa Tags HTML?
